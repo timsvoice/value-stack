d6348cd9a8d51ea38700c4b097ac2f721b9b9b87
Timothy Voice <timsethvoice@gmail.com>
2016-11-04 19:03:16 -0400

    added player attr to schema

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -2,13 +2,25 @@ import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './resolvers';
 
 const schema = `
-  type Post {
-    name: String
+  type Player {
+    playerId: String
+    first_name: String
+    fppg: Number
+    game: String
+    injury_details: String
+    injury_indicator: String
+    last_name: String
+    opponent: String
+    played: Number
+    playerId: String
+    position: String
+    salary: Number
+    team: String
   }
 
   # the schema allows the following query:
   type RootQuery {
-    post: Post
+    players: [Player]
   }
 
   # we need to tell the server which types represent the root query
